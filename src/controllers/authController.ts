db021d552413f22e8e5799b86ef53e5344915c41
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-09 13:58:11 +0530

    create login controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -32,3 +32,28 @@ export const register = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const normalizedEmail = email.trim().toLowerCase();
+
+    const existingUser = await User.findOne({ email: normalizedEmail });
+
+    if (!existingUser) {
+      return res.status(400).json({ message: "Email doesn't exists" });
+    }
+
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+
+    if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+    return res.status(200).json({
+      message: "Login successful",
+      user: { id: existingUser.id, email: existingUser.email },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
