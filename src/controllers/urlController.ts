46fff813e3f658cea828a2526291640eb34c4213
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-10 19:51:54 +0530

    add generate url

--- src/controllers/urlController.ts
+++ src/controllers/urlController.ts
@@ -2,5 +2,9 @@ import { Request, Response } from "express";
 
 export const shortUrl = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const { originalUrl } = req.body;
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
 };
