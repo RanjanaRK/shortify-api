d259a4f465bf6a13b47a499f87e6dbdfa224be05
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-11 16:49:56 +0530

    Update qrController.ts

--- src/controllers/qrController.ts
+++ src/controllers/qrController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import QRCode from "qrcode";
+import { nanoid } from "nanoid";
+import { Url } from "../models/Url";
 
 export const generateQrCode = async (req: Request, res: Response) => {
   try {
@@ -12,13 +14,38 @@ export const generateQrCode = async (req: Request, res: Response) => {
       });
     }
 
-    // Generate QR Code as Base64
-    const qrImage = await QRCode.toDataURL(originalUrl);
+    const existing = await Url.findOne({ originalUrl });
+
+    if (!existing) {
+      const shortCode = nanoid(10);
+
+      const newUrl = await Url.create({
+        originalUrl,
+        shortCode,
+        createdBy: req.user?.id || null,
+      });
+
+      const shortUrl = `${process.env.BASE_URL}/${newUrl.shortCode}`;
+
+      const qrImage = await QRCode.toBuffer(shortUrl);
+
+      return res.status(200).json({
+        success: true,
+        message: "QR Code generated, generated shorURl right now",
+        qr: qrImage,
+        shortUrl,
+        originalUrl,
+      });
+    }
+
+    const finalShortUrl = `${process.env.BASE_URL}/${existing.shortCode}`;
+    const qrImage = await QRCode.toDataURL(finalShortUrl);
 
     return res.status(200).json({
       success: true,
-      message: "QR Code generated",
-      qr: qrImage, // Base64 image string
+      message: "QR Code generated , short url already generated",
+      qr: qrImage,
+      shortUrl: finalShortUrl,
       originalUrl,
     });
   } catch (error: any) {
