5063dd08ccca787cd7b253d568b1622a255a2657
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-22 22:01:20 +0530

    Update rateLimiter.ts

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -3,8 +3,13 @@ import { rateLimit } from "express-rate-limit";
 export const limiter = rateLimit({
   windowMs: 60 * 1000,
   limit: 3,
-  message: "Too many requests",
   standardHeaders: "draft-8",
   legacyHeaders: false,
   ipv6Subnet: 56,
+  handler: (req, res) => {
+    return res.status(429).json({
+      code: "LOGIN_RATE_LIMIT",
+      message: "Too many login attempts.",
+    });
+  },
 });
