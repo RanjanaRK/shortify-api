daa2659eea31015c08045d2e497a66f1b537c588
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-23 23:12:21 +0530

    Update auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -25,25 +25,6 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-// export const auth = async (req: Request, res: Response, next: NextFunction) => {
-//   const authHeader = req.headers.authorization;
-//   const token = authHeader?.split(" ")[1];
-
-//   if (!token) {
-//     return res.status(401).json({ message: "Unauthorized: No token" });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
-//       id: string;
-//     };
-//     (req as any).user = { id: decoded.id }; // only store the user id
-//     next();
-//   } catch (err) {
-//     return res.status(401).json({ message: "Invalid token" });
-//   }
-// };
-
 export const optionalAuth = (
   req: Request,
   res: Response,
