9a77ceb7916b23319d8d600498faf7d38423bd4e
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-23 22:00:55 +0530

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,18 @@
 import express from "express";
-import { currentUser, getAllUsers } from "../controllers/userController";
-import { auth } from "../middlewares/auth";
+import {
+  currentUser,
+  getAllUsers,
+  getUserActivity,
+} from "../controllers/userController";
+import { auth, optionalAuth } from "../middlewares/auth";
+import { checkAnonUser } from "../middlewares/checkAnonId";
 
 const router = express.Router();
 
 router.get("/me", auth, currentUser);
 
 router.get("/users", auth, getAllUsers);
+
+router.get("/activity", optionalAuth, checkAnonUser, getUserActivity);
 
 export default router;
