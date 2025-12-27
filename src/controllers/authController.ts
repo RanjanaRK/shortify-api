07eb0f8ec8d1fd8e3ea2baa27fff194395712624
RanjanaRK <ranjanakumari.me@gmail.com>
2025-12-27 20:58:04 +0530

    Update authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -4,6 +4,8 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/User";
 import { Url } from "../models/Url";
 
+// USER REGISTRATION
+
 export const register = async (req: Request, res: Response) => {
   try {
     const { name, email, password } = req.body;
@@ -51,6 +53,8 @@ export const register = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
+
+// lOGIN
 
 export const login = async (req: Request, res: Response) => {
   const expiryDate = new Date(Date.now() + 60 * 60 * 1000);
@@ -109,6 +113,66 @@ export const login = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
+
+// export const login = async (req: Request, res: Response) => {
+//   const expiryDate = new Date(Date.now() + 60 * 60 * 1000);
+
+//   try {
+//     const { email, password } = req.body;
+//     const normalizedEmail = email.trim().toLowerCase();
+
+//     const existingUser = await User.findOne({ email: normalizedEmail });
+
+//     if (!existingUser) {
+//       return res.status(400).json({ message: "Email doesn't exists" });
+//     }
+
+//     const isMatch = await bcrypt.compare(password, existingUser.password);
+
+//     if (!isMatch) return res.status(400).json({ message: "Invalid password" });
+
+//     const token = jwt.sign({ id: existingUser.id }, process.env.JWT_SECRET!, {
+//       expiresIn: "1h",
+//     });
+
+//     const anonId = req.anonId;
+
+//     if (anonId) {
+//       const abc = await Url.updateMany(
+//         {
+//           anonId: req.anonId,
+//         },
+//         {
+//           $set: { createdBy: existingUser.id },
+//           $unset: { anonId: "" },
+//         }
+//       );
+//     }
+
+//     res.clearCookie("anon-id", {
+//       httpOnly: true,
+//       sameSite: "lax",
+//     });
+
+//     res.cookie("jwt-token", token, {
+//       httpOnly: true,
+//       secure: true,
+//       sameSite: "lax",
+//       expires: expiryDate,
+//       path: "/",
+//     });
+
+//     return res.status(200).json({
+//       message: "Login successful",
+//       user: { id: existingUser.id, email: existingUser.email },
+//     });
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ message: "Server error" });
+//   }
+// };
+
+// lOGOUT
 
 export const logout = async (req: Request, res: Response) => {
   try {
