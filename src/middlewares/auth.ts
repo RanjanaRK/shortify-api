4c060aede6ac49d7c3cc1a09d7f9728b996f5671
RanjanaRK <ranjanakumari.me@gmail.com>
2026-01-04 23:56:59 +0530

    Update auth.ts

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,37 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
-// export const auth = async (req: Request, res: Response, next: NextFunction) => {
-//   const token = req.headers.authorization?.split(" ")[1];
-
-//   try {
-//     if (req.headers.authorization === null) {
-//       req.user = null;
-//       return next();
-//     }
-
-//     if (!token)
-//       return res.status(401).json({ message: "Unauthorized: No token" });
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
-//       id: string;
-//     };
-//     (req as any).user = decoded;
-
-//     next();
-//   } catch {
-//     res.status(401).json({ message: "Invalid token" });
-//     req.user;
-//   }
-// };
-
 export const requireAuth = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
-    // const token = req.headers.authorization?.split(" ")[1];
     const token = req.cookies.access_token;
     console.log("CookiesToken:", token);
 
@@ -51,36 +26,6 @@ export const requireAuth = async (
   }
 };
 
-// export const optionalAuth = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const accessToken = req.cookies.access_token;
-//   const refreshToken = req.cookies.refresh_token;
-
-//   console.log(accessToken, ":optionalauthatoekn");
-//   console.log(refreshToken, ":optionrefreshtoken");
-//   if (!refreshToken || !accessToken) {
-//     req.user = null;
-//     return next();
-//   }
-
-//   try {
-//     const decoded = jwt.verify(accessToken, process.env.JWT_SECRET!);
-//     req.user = decoded;
-//     return next();
-//   } catch {
-//     if (!refreshToken) {
-//       req.user = null;
-
-//       return next();
-//     }
-//   }
-
-//   next();
-// };
-
 export const optionalAuth = async (
   req: Request,
   res: Response,
@@ -89,7 +34,7 @@ export const optionalAuth = async (
   const accessToken = req.cookies.access_token;
   const refreshToken = req.cookies.refresh_token;
 
-  // No refresh → definitely anonymous
+  // if no refresh then definitely anonymous
   if (!refreshToken) {
     req.user = null;
     return next();
